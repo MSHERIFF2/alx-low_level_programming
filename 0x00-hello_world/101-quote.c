26adc80701392ff292166865202af819444eadd4
MSHERIFF2 <muritalasheriffdeenadegoke@gamil.com>
2023-07-13 16:15:22 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,11 @@
 
 /**
  * main - quote
- *
- * Description: 101-quote
- * Return: 1
+ * Return:1
  */
-
 int main(void)
 {
-	fputs(" and that piece of art is useful\" - Dora Korpar, 205-10-19\n",
-			stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		stdout);
 	return (1);
 }
