d5b8843cb973d2defbab2ea621d2f137f2682654
MSHERIFF2 <muritalasheriffdeenadegoke@gamil.com>
2023-07-13 15:19:56 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - quote
+ *
+ * Description: 101-quote
+ * Return: 0 (sucess)
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 205-10-19\n", stdout);
+	return (1);
+}
