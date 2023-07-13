c4001a54c189c6239c8626884e0b62eb0fbe1e5b
MSHERIFF2 <muritalasheriffdeenadegoke@gamil.com>
2023-07-13 10:12:49 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - hello func
+ *
+ * Description: print hello
+ * Return: always zero
+ */
+
+int main(void)
+{
+	int sentece = "Programming is like building a multilingual puzzle";
+
+	puts(sentece);
+	return (0);
+}
