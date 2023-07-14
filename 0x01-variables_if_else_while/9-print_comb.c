5836499f5b44a3f73cb6b8cf7d21daabb2249bfe
MSHERIFF2 <muritalasheriffdeenadegoke@gamil.com>
2023-07-14 16:24:17 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,17 @@
 
 int main(void)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < 10; i++)
+	while (i < 10)
 	{
-		putchar(',');
-		putchar(' ');
 		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
 	}
 	putchar('\n');
 	return (0);
