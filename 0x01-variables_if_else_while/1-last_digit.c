8f7c74420c42b8cc6bb04a9209c63fec6a6a6c27
MSHERIFF2 <muritalasheriffdeenadegoke@gamil.com>
2023-07-14 10:29:00 -0700

    1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,8 +23,8 @@ int main(void)
 		printf("and is greater than 5");
 	if (m == 0)
 		printf("and is 0");
-	if (m < 6 && n != 0)
-		printf("and is less than 6 and not 0");
+	if (m < 6 && m != 0)
+		printf(" and is less than 6 and not 0");
 	printf("\n");
 	return (0);
 }
