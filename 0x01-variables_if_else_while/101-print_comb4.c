a51920cb07d2c510e31f5609d63b4899246f0667
MSHERIFF2 <muritalasheriffdeenadegoke@gamil.com>
2023-07-15 00:05:49 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,9 +16,9 @@ int main(void)
 		{
 			for (k = j + 1; k <= 9; k++)
 			{
-				putchar('0');
-				putchar('0');
-				putchar('0');
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
 				if (i != 7 || j != 8 || k != 9)
 				{
 					putchar(',');
