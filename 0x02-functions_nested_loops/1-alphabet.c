832499ff4b8a549c99050369f987aec1e120600d
MSHERIFF2 <muritalasheriffdeenadegoke@gamil.com>
2023-07-18 13:07:26 -0700

    create alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+	return (0);
+}
