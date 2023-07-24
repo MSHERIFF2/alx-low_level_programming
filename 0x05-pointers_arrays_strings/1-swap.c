959fcdd6b81f59c24620f6c8515f553ef3e5f035
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2023-07-24 18:56:53 +0100

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+
+#include "main.h"
+
+/**
+ * swap_int - this swap the value of two pointers
+ *
+ * @a: an address in a memory
+ * @b: an address in a memroy
+ * Return: always 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
