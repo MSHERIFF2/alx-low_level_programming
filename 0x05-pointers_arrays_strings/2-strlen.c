29bca27cce4952f726e6d233dd52e983db69d733
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2023-07-24 21:19:23 +0100

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - printout the length of a string
+ *
+ * @s : the pointer for the string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
