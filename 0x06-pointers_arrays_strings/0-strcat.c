db0e253a47171e0c4842e070b974c397ec46b4a7
MSHERIFF2 <muritalasheriffdeenadegoke@gamil.com>
2023-07-27 14:39:56 -0700

    strcat.c editied

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,6 +10,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest, src);
-	return (0);
+	int l1;
+	int l2;
+
+	l1 = 0;
+	l2 = 0;
+	while (*(dest + l1) != '\0')
+		l1++;
+	while (*(src + l2) != '\0' &&  l1 < 97)
+	{
+		*(dest + l1) = *(src + l2);
+		l1++;
+		l2++;
+	}
+	*(dest + l1) = '\0';
+	return (dest);
 }
