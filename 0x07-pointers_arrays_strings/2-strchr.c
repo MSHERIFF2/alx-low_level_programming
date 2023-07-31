acc19be3956fe32c4422b6670fa744acaa4105e8
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2023-07-31 22:47:03 +0100

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character in a string
+ * @s: pointer to the string
+ * @c: the character to locate
+ * Return: 1
+ */
+char *_strchr(char *s, char c)
+{
+	for (; *s != '\0'; s++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	}
+	return (NULL);
+}
