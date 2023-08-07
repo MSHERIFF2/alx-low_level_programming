89ee4b34a01634a2057c657f1da8a9ac2b4cba48
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2023-08-07 15:14:35 +0100

    0-main.c

---
+++ 0x0B-malloc_free/0-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *buffer;
+
+	buffer = create_array(98, 'H');
+	if (buffer == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (1);
+	}
+	create_array(buffer, 98);
+	free(buffer);
+	return (0);
+}
