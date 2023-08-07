f66f12539e3701f648f98610dac4d7d54a89be9b
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2023-08-07 23:22:42 +0100

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,15 +12,15 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *concatinate;
+	size_t lent1 = strlen(s1);
+	size_t lent2 = strlen(s2);
 
-	concatinate = malloc(strcat(s1, s2));
-	if (concatinate = NULL)
-	{
-		return (" ");
-	}
-	if (concatinate < NULL)
+	concatinate = malloc(lent1 + lent2 + 1);
+	if (concatinate == NULL)
 	{
 		return (NULL);
 	}
+	strcpy(concatinate, s1);
+	strcat(concatinate, s2);
 	return (concatinate);
 }
