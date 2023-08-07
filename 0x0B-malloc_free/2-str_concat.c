b0d0d54487e1135d6379c5b12ea5aed65ab8d62f
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2023-08-07 23:54:42 +0100

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,18 +11,18 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
 	char *concatinate;
 	size_t lent1 = strlen(s1);
 	size_t lent2 = strlen(s2);
 
+	if (s1 == NULL)
+	{
+		s1 = " ";
+	}
+	if (s2 == NULL)
+	{
+		s2 = " ";
+	}
 	concatinate = (char *)malloc(lent1 + lent2 + 1);
 	if (concatinate == NULL)
 	{
