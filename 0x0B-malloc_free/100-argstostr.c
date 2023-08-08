2724d1fd211df001775bcc24c9bb4e1b5208c0ab
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2023-08-08 17:55:27 +0100

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,71 +1,41 @@
-#include "main.h"
+#include "main.h" 
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * _strcat - Concat string, src appended to dest
- *
- * @dest: Destination string
- * @src: Source string
- * Return: Concat newString to dest
- */
-char *_strcat(char *dest, char *src)
-{
-        int a, b;
-
-        for (a = 0; dest[a] != '\0'; a++)
-                ;
-        for (b = 0; src[b] != '\0'; b++)
-        {
-                dest[a] = src[b];
-                a++;
-        }
-        return (dest);
-}
-
-/**
- * _strlen - Return the number of the length of the string
- *
- * @s: This is the string to check
- *
- * Return: Return the length of the string
- */
-int _strlen(char *s)
-{
-	int a;
-
-	for (a = 0; s[a] != '\0'; a++)
-		;
-	return (a);
-}
-
-/**
- * argstostr - Concats argv to new string
- *
- * @ac: Argument count
- * @av: Arguments
- *
- * Return: Returns a pointer to a new string, or NULL if it fails
+ * argstostr - function that concatenates all the arguments
+ * @ac: variable 
+ * @av: pointer
+ * Return: pointer
  */
 
 char *argstostr(int ac, char **av)
 {
-	int index, sumString;
-	char *newString;
-
-	sumString = 1;
-
 	if (ac == 0 || av == NULL)
-		return (0);
-	for (index = 0; index < ac && av[index]; index++)
 	{
-		sumString += _strlen(av[index]);
+		return NULL;
 	}
-	newString = malloc((sumString * sizeof(char)) + ac);
-	for (index = 0; index < ac && av[index]; index++)
+	int total_length = 0;
+	
+	for (int i = 0; i < ac; i++)
 	{
-		newString = _strcat(newString, av[index]);
-		newString = _strcat(newString, "\n");
+		total_length += strlen(av[i]) + 1;
 	}
-	if (newString == NULL)
-		return (NULL);
-	return (newString);
+	char *concatenated = (char *)malloc(total_length * sizeof(char));
+	
+	if (concatenated == NULL)
+	{
+		return NULL;
+	}
+	int position = 0;
+	
+	for (int i = 0; i < ac; i++)
+	{
+		strcpy(concatenated + position, av[i]);
+		position += strlen(av[i]);
+		concatenated[position] = '\n';
+		position++;
+	}
+	return concatenated;
 }
+
