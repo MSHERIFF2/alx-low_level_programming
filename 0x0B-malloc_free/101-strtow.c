40136ae45da54eae261808f904150cc43aba2f5f
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2023-08-08 20:16:07 +0100

    101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * strtow - function that splits a string into words
+ * @str: pointer to the string
+ * Return: pointer to an array
+ */
+char **strtow(char *str)
+{
+	int j;
+
+	if (str == NULL || str == "")
+	{
+		return (NULL);
+	}
+	for (j = 0; j < str; j++)
+	{
+		str[j] + 1;
+		_putchar(str);
+	}
+	return (str);
+}
