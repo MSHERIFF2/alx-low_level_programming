16624731ed438bb8e07e3f153fcc037dab7cc123
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2023-08-08 18:01:31 +0100

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,25 +11,28 @@
 
 char *argstostr(int ac, char **av)
 {
+	int total_length = 0;
+	int i;
+	char *concatenated;
+	int position;
+
 	if (ac == 0 || av == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
-	int total_length = 0;
-	
-	for (int i = 0; i < ac; i++)
+	for (i = 0; i < ac; i++)
 	{
 		total_length += strlen(av[i]) + 1;
 	}
-	char *concatenated = (char *)malloc(total_length * sizeof(char));
+	concatenated = (char *)malloc(total_length * sizeof(char));
 	
 	if (concatenated == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
-	int position = 0;
+	position = 0;
 	
-	for (int i = 0; i < ac; i++)
+	for (i = 0; i < ac; i++)
 	{
 		strcpy(concatenated + position, av[i]);
 		position += strlen(av[i]);
