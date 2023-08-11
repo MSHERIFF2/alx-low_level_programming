48140e8729f3eac7dcbb92f7b846db920b21f277
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2023-08-11 13:41:54 +0100

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,7 +14,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;
-	unsigned int copy_size = (old_size) ? old_size : new_size;
+	unsigned int copy_size;
 
 	if (new_size == 0)
 	{
@@ -23,14 +23,17 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (ptr == NULL)
 	{
-		return (malloc(new_size));
+		void *cond;
+		cond = malloc(new_size);
+		return (cond);
 	}
 	if (new_size == old_size)
 		return (ptr);
+	copy_size = (olde_size < new_size) ? old_size : new_size;
 	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 		return (NULL);
-	memcpy(new_ptr, ptr, old_size);
+	memcpy(new_ptr, ptr, copy_size;
 	free(ptr);
 	return (new_ptr);
 }
