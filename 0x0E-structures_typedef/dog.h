c56e754cad0145e4d67c5dc75ad1edfdbb4f6830
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2023-08-15 03:19:03 +0100

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef MYSTRUCT_H
 #define MYSTRUCT_H
 
+/**
+ * struct dog - file define element of dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: Details about a dog
+ */
 struct dog
 {
 	char *name;
