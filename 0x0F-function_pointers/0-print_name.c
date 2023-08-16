a8b0f63f8ffc0eda629805ac1da5f50607c3f17a
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2023-08-16 22:18:59 +0100

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: a pointer to character
+ * @f: a function pointer to character
+ *
+ * Return: a character
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f = name;
+}
