23da60098b44a0cce7708ccfdd79768f4d0d437f
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2023-08-24 15:08:57 +0100

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,7 +14,7 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)");
+			printf("[%u] (nil)", h->len);
 		else
 			printf("[%u] %s\n", h->len, h->str);
 			count += 1;
