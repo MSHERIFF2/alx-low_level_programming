fb898c3ab6c3344892dda064425d0dbda40e9a17
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2023-08-28 21:22:40 +0100

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,11 +14,10 @@ size_t print_listint(const listint_t *h)
 	while (h != NULL)
 	{
 		if (h->n == 0)
-			return(0);
-		else
-			printf("%d\n", h->n);
-		++count;
+			return (0);
+		printf("%d\n", h->n);
 		h = h->next;
+		count++;
 	}
 	return (count);
 }
