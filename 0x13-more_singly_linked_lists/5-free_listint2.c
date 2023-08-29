9936e9fb04b214b20bb6aafa906c8135a1175896
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2023-08-30 00:01:38 +0100

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,8 +7,8 @@
  */
 void free_listint2(listint_t *head)
 {
-	if (head == NULL)
-		return;
+	*head = NULL;
+	
 	while ((*head) != NULL)
 	{
 		listint_t *tmp;
@@ -17,5 +17,4 @@ void free_listint2(listint_t *head)
 		(*head) = (*head)->next;
 		free(tmp);
 	}
-	(*head) = NULL;
 }
