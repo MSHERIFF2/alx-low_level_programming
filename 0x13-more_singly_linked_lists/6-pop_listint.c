ade9a70d58fb717a40a7a16507f9784d1c728ba8
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2023-08-30 19:20:28 +0100

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -12,9 +12,12 @@ int pop_listint(listint_t **head)
 
 	if (*head == 0)
 		return (0);
-	temp = (*head);
-	(*head) = (*head)->next;
-	free(temp);
+	while ((*head) != NULL)
+	{
+		temp = (*head);
+		(*head) = (*head)->next;
+		free(temp);
+	}
 	return (0);
 }
 
