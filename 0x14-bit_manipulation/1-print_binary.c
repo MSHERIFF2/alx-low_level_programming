7dadc92873f6351018203cbf2db0a98954489d7d
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2023-09-01 00:12:14 +0100

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,15 +10,26 @@ void print_binary(unsigned long int n)
 {
 	int num_bits = sizeof(unsigned long int) * 8;
 	int i;
+	int leading_zeros = 1;
 
+	if (n == 0)
+	{
+		printf("0\n");
+		return;
+	}
 	for (i = num_bits - 1; i >= 0; i--)
 	{
 		unsigned long int mask = 1UL << i;
 
 		if (n & mask)
+		{
+			leading_zeros = 0;
 			printf("1");
-		else
+		}
+		else if (!leading_zeros)
+		{
 			printf("0");
+		}
 	}
 	printf("\n");
 }
