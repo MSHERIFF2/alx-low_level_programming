c876bbfec200a1d744f958fbf4045d8898312c23
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2023-09-02 00:40:35 +0100

    100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -10,7 +10,7 @@ int get_endianness(void)
 	unsigned int num = 1;
 	unsigned char *byte_ptr = (unsigned char *)&num;
 
-	if (*byte_ptr == 1) 
+	if (*byte_ptr == 1)
 		return (1);
 	else
 		return (0);
