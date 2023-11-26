a0536699df12ad363e2af87828048d7e8dab8f6b
MSHERIFF2 <muritalasheriffdeenadegoke@gmail.com>
2023-11-26 11:27:30 +0100

    task 0 done

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * binary_to_uint - function that convert binary to decimal
+ * @b: pointer to the binary character
+ *
+ * Return: decimal value
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i, decimal, length;
+
+	decimal = 0;
+	length = strlen(b);
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	for (i = 0; i < length; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+	}
+	for (i = 0; i < length; i++)
+	{
+		decimal = decimal * 2 + (b[i] - '0');
+	}
+	return (decimal);
+}
